efe497af425a59f140ab19e209f7af2f0dd30e28
Rounaq Jhunjhunu wala <rounaq14089@iiitd.ac.in>
2026-06-18 16:14:32 +0530

    Add: ransom-note - submission-0

---
+++ Data Structures & Algorithms/ransom-note/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] cnt = new int[26];
+        for (char ch : magazine.toCharArray()) {
+            cnt[ch - 'a'] ++;
+        }
+        for (char ch : ransomNote.toCharArray()) {
+            if (cnt[ch - 'a'] <= 0) return false;
+            cnt[ch - 'a'] --;
+        }
+        return true;
+    }
+}
